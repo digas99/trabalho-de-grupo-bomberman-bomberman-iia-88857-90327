5b0873d05d67f55c41356d65e3fb117d27d41577
Henrique Silva <henrique>
2019-11-26 18:34:45 +0000

    otimizacao do codigo para o nivel 1, precisa levar merge com o codigo do nivel 2

--- student2.py
+++ student2.py
@@ -27,25 +27,29 @@ async def agent_loop(server_address="localhost:8000", agent_name="student"):
 
         # You can create your own map representation or use the game representation:
         mapa = Map(size=game_properties["size"], mapa=game_properties["map"])
-        
+    
         last_key = ""
         deployed_bomb_counter = 0
         destroyed_walls = []
         has_deployed = False
         after_deploy = False
-        destiny_wall = None
+        destiny = None
         last_block = "0, 0"
         before_last_block = "0, 0"
         stepCount = 0
         array_keys= []
-        powerup_discovered = False
+        powerup_discover = False
         level = 0
         count_powerups = 0
         powerup_picked_up = False
+        current_state = 0
+        
 
 
         while True:
             try:
+                print(" ")
+                print("BEGINNING OF LOOP!!!")
                 
                 state = json.loads(
                     await websocket.recv()
@@ -63,26 +67,43 @@ async def agent_loop(server_address="localhost:8000", agent_name="student"):
                     destroyed_walls = []
                     has_deployed = False
                     after_deploy = False
-                    destiny_wall = None
+                    destiny = None
                     last_block = "0, 0"
                     before_last_block = "0, 0"
                     stepCount = 0
                     array_keys= []
                     powerup_discover = False
                     has_powerup = count_powerups
+                    balloom_spotted = False
+                    wall_spotted = False
+                    balloom_in_range = None
+                    
 
                 bomberman = state['bomberman']
                 powerup = state["powerups"]
+
+                enem = state["enemies"] #Guarda os inimigos num array
+                enem_coords = [c['pos'] for c in enem] #Guarda as coordenadas de cada balloom
                 
+                balloom_spotted = False
 
                 if powerup != []:
                     powerup_discover = True
                 
                 elif powerup == [] and powerup_discover:
                     count_powerups += 1
+                    powerup_picked_up = True
+                    
+                if (state['walls']== [] and state['enemies'] != [] and array_keys==[]):
                 
-                if (stepCount < 308 and array_keys==[]) or (state['walls']== [] and state['enemies'] != [] and array_keys==[]):
                     if bomberman != [1,1]:
+
+                        if has_powerup:
+                            if same_line(bomberman, closest_enemy(bomberman, enem_coords)) and (distance_to(bomberman, closest_enemy(bomberman, enem_coords)) < 7):
+                                key = "B"
+                        else:
+                            if same_line(bomberman, closest_enemy(bomberman, enem_coords)) and (distance_to(bomberman, closest_enemy(bomberman, enem_coords)) < 4):
+                                key = "B"
 
                         blocks = get_blocks(mapa, bomberman, [1,1])
                         coordinates = get_coords(blocks)
@@ -101,8 +122,21 @@ async def agent_loop(server_address="localhost:8000", agent_name="student"):
 
                         else:
                             array_keys = ["d","d","B","a","a","s","","","","","w"]
+
+
+                if powerup_discover and not powerup_picked_up:
+                        blocks = get_blocks(mapa, bomberman, state["powerups"][0][0])
+                        coordinates = get_coords(blocks)
+                        conexions = get_conexions(blocks)
+                        connections = Connections(conexions, coordinates)
+                        p = SearchProblem(connections, to_string(bomberman), to_string(state["powerups"][0][0]))
+                        t = SearchTree(p,'a*')
+                        result = t.search(90)
+                        array_keys = path_to_array_keys(result[0])
+                        
                 
-                elif state["exit"] != [] and state["enemies"] == [] and array_keys == []:
+
+                elif state["exit"] != [] and state["enemies"] == [] and array_keys == [] and powerup_picked_up:
                         blocks = get_blocks(mapa, bomberman, state["exit"])
                         coordinates = get_coords(blocks)
                         conexions = get_conexions(blocks)
@@ -111,47 +145,46 @@ async def agent_loop(server_address="localhost:8000", agent_name="student"):
                         t = SearchTree(p,'a*')
                         result = t.search(90)
                         array_keys = path_to_array_keys(result[0])
+
                     
 
-                elif stepCount >= 308 and state['walls']!= []:    
-
+                elif state['walls']!= [] or array_keys == []:   
                     # if there are destroyed walls, then don't add them to the walls we want
                     walls = [w for w in state['walls'] if w not in destroyed_walls]
-                    #walls = state['walls']
                     bomberman_string = to_string(bomberman)
-            
-
+                    print("AfterDeploy")
+                    print(after_deploy)
+                    print("len(walls)")
+                    print(len(walls))
                     if(len(walls) != 0):
                         if (not after_deploy):
-                            destiny_wall = closest_wall(bomberman, walls)
+                            destiny = closest_wall(bomberman, walls)
+                            print("destiny")
+                            print(destiny)
                             after_deploy = True
                         else: 
                             next_block = "1, 0"
 
-                    blocks = get_blocks(mapa, bomberman, destiny_wall)
+                    blocks = get_blocks(mapa, bomberman, destiny)
                     coordinates = get_coords(blocks)
                     conexions = get_conexions(blocks)
-
                     connections = Connections(conexions, coordinates)
-    
-                    p = SearchProblem(connections, bomberman_string, to_string(destiny_wall))
+                    p = SearchProblem(connections, bomberman_string, to_string(destiny))
                     t = SearchTree(p,'a*')
-
                     result = t.search(90)
 
                     print(result)
-                
-
                     print(state)
 
                     print("")
+                    print("enemie coord: ")
+                    print(enem_coords)
+                    print("Closeste enemie: ")
+                    print(closest_enemy(bomberman, enem_coords))
                     print ("Bomberman: ")
                     print (bomberman)
                     print ("Closest Wall: ")
                     print (closest_wall(bomberman, walls))
-                
-                
-
                     print("Path: ")
                     print(result)            
 
@@ -163,14 +196,8 @@ async def agent_loop(server_address="localhost:8000", agent_name="student"):
                 
                     before_last_block = last_block
                     last_block = next_block
-                
-                    #if has discovered the exit, then go for it
-                    #if (len(state["exit"]) > 0):
-                    #    next_block = to_string(state["exit"])
 
-                    
-                
-                    # let bomberman be in the same position for some frames, to be protected form bomb
+                    # let bomberman be in the same position for some frames, to be protected from bomb
                     if(count_powerups>0):
                         if (deployed_bomb_counter == 10):
                             after_deploy = False
@@ -182,7 +209,16 @@ async def agent_loop(server_address="localhost:8000", agent_name="student"):
 
                     if (deployed_bomb_counter == 0):
                         key = get_key(bomberman_string, next_block)
-    
+
+
+                    #kill balloom
+                    if has_powerup and enem != []:
+                        if same_line(bomberman, closest_enemy(bomberman, enem_coords)) and (distance_to(bomberman, closest_enemy(bomberman, enem_coords)) < 7):
+                            key = "B"
+                    elif not has_powerup and enem != []:
+                        if same_line(bomberman, closest_enemy(bomberman, enem_coords)) and (distance_to(bomberman, closest_enemy(bomberman, enem_coords)) < 4):
+                            key = "B"
+
                     # check when bomberman get close to the destiny wall and deploy bomb
                     if (result != None and len(result[0]) == 2):
                         key = "B"
@@ -190,7 +226,8 @@ async def agent_loop(server_address="localhost:8000", agent_name="student"):
 
                     # run from bomb
                     if (last_key == "B" or deployed_bomb_counter == 1):
-                        key = away_from_wall(bomberman, destiny_wall)
+                        if(destiny != None):
+                            key = away_from_wall(bomberman, destiny)
                         deployed_bomb_counter += 1
                 
                     if (deployed_bomb_counter > 1):
@@ -201,11 +238,11 @@ async def agent_loop(server_address="localhost:8000", agent_name="student"):
                             if (deployed_bomb_counter == 2):
                                 key = last_key
                             elif (deployed_bomb_counter == 3):
-                                key = change_key_randomly(last_key, bomberman, destiny_wall, walls, deployed_bomb_counter)
+                                key = change_key_randomly(last_key, bomberman, destiny, walls, deployed_bomb_counter)
                             else:
                                 key = ""
                         else:
-                            key = change_key_randomly(last_key, bomberman, destiny_wall, walls, deployed_bomb_counter)
+                            key = change_key_randomly(last_key, bomberman, destiny, walls, deployed_bomb_counter)
                         deployed_bomb_counter += 1
 
 
@@ -235,6 +272,20 @@ def distance_to(obj1, obj2):
 
     return distance
 
+def closest_enemy(bombermanPos, enem):
+
+    dist_min = 123456789
+
+    for i in range(len(enem)):
+
+        distancia = distance_to(bombermanPos, enem[i])
+
+        if(distancia < dist_min):
+
+            dist_min = distancia
+            minEnemy = enem[i]
+
+    return minEnemy
 
 def closest_wall(bombermanPos, walls): #entradas sao o bomberman e array de walls
 
@@ -391,6 +442,16 @@ def opposite_key(key):
         return "a"
     if (key == "s"):
         return "w"
+
+def in_range(entity1, entity2, range_val): #vê tudo o que está no raio range_val
+    if (abs(entity1[0] - entity2[0]) <= range_val and abs(entity1[1] - entity2[1]) <= range_val):
+        return True
+    return False
+
+def same_line(entity1, entity2): #ve se o bomberman e o enemy estao no mesmo eixo
+    if entity1[0] == entity2[0] or entity1[1] == entity2[1]:
+        return True
+    return False
 
 # DO NOT CHANGE THE LINES BELLOW
 # You can change the default values using the command line, example:
